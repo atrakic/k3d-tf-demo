a0b08847c106afdbd2d18bcf7d1d6245e084b7d6
Admir Trakic <atrakic@users.noreply.github.com>
2021-04-15 23:02:13 +0200

    Add local Chart

--- main.tf
+++ main.tf
@@ -1,13 +1,25 @@
 locals {
-  name = "k3d-tf-demo"
+  name        = "k3d-tf-demo"
+  config_path = "~/.kube/config"
 }
 
 provider "kubernetes" {
-  config_path = "~/.kube/config"
+  config_path = local.config_path
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = local.config_path
+  }
 }
 
 resource "kubernetes_namespace" "this" {
   metadata {
     name = local.name
   }
+}
+
+resource "helm_release" "kubewatch" {
+  name  = local.name
+  chart = "./Charts/demo"
 }
