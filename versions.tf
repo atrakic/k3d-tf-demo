6c5c6c3619105ac3f6f86bb53c4c150873f89810
Admir Trakic <atrakic@users.noreply.github.com>
2021-08-15 06:57:38 +0200

    Bump k3d-action

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.12.6"
+  required_version = "~> 0.14"
 
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.2.0"
+      version = "2.4.1"
     }
   }
 }
